df345103ff27ee6fbc629be2193e8a5953a590ab
vlasmar <40355727+vlasmar@users.noreply.github.com>
2022-10-13 16:35:08 +0200

    add style at header and footer

---
+++ kidify/src/Components/UserProfile.js
@@ -0,0 +1,12 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
+function UserProfile () {
+    <div>
+        <Header />
+        
+        <Footer />
+    </div>
+}
+
+export default UserProfile;
