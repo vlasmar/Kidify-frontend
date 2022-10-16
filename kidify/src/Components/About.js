d557472bf91f18b6818cb4694c6f40da0c47e8ea
Priyanka_Bed <108406975+PriyankaBed@users.noreply.github.com>
2022-10-16 20:47:42 +0200

    about added
    about added

--- kidify/src/Components/About.js
+++ kidify/src/Components/About.js
@@ -1,6 +1,8 @@
 import Header from "./Header";
 import Footer from "./Footer";
-import Logo from "../Images/kidify-logo.png";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Team1 from "../Images/team_1.jpg";
 
 function About({ medium }) {
     return (
@@ -16,17 +18,20 @@ function About({ medium }) {
             >
                 <div
                     style={{
-                        marginBottom: "50px",
-                        marginTop: "50px",
+                        marginBottom: "13px",
+                        marginTop: "25px",
                         width: "70%",
                         color: "black",
-                        fontSize: "25px",
+                        fontSize: "22px",
                     }}
                 >
-                    <h1 style={{ fontFamily: "cursive" }}>Our mission</h1>
-                    <p>is to create a magic portal for you to watch videos for children from all around the web and create your own playlists with as few clicks as possible.</p>
-                    <p>Create and play your own playlists and relax. It's literally kids' play.</p>
-                    <p>You can watch all provided videos and create temporary playlists without even signing up. But behold: as soon as you log out, your portal will close. If you want Lullifey to maintain your portal, sign up!</p>
+                    <div className='about_mission'>
+                        <h1 style={{ fontFamily: "cursive" }}>Our mission</h1>
+                    </div>
+
+                    <p>is to Create a website with fun educational videos for kids. you to watch fun educational videos for children from all around the web and create your own playlists with as few clicks as possible. Create and play your own playlists and relax.</p>
+                    <p>Our vision is for the website to be the source of childhood learning on the internet.</p>
+                    <p>You can watch all provided videos and create temporary playlists without even signing up. But behold: as soon as you log out, your portal will close. If you want kidify to maintain your portal, sign up!</p>
                 </div>
             </div>
 
@@ -39,20 +44,41 @@ function About({ medium }) {
                 {
                     <div
                         style={{
-                            marginBottom: "30px",
-                            marginTop: "20px",
+                            marginBottom: "20px",
+                            marginTop: "18px",
                             width: "70%",
                             color: "black",
                             fontSize: "20px",
                         }}
                     >
                         <h1 style={{ fontFamily: "cursive" }}>Meet Our Team</h1>
-                        <img className='logo' src={Logo} alt='logo' />
-                        <img className='logo' src={Logo} alt='logo' />
+                        <div className='aboutcards'>
+                            <div className='aboutcard_1'>
+                                <Card style={{ width: "18rem" }}>
+                                    <Card.Img variant='top' src={Team1} alt='team1' />
+                                    <Card.Body>
+                                        <Card.Title>Priyanka Bed</Card.Title>
+                                        <Card.Text>Full Stack Developer</Card.Text>
+                                        <Card.Link href='https://www.linkedin.com/in/priyankaben-bed/'>Linkedin</Card.Link>
+                                        <Card.Link href='https://github.com/PriyankaBed'>Gith hub</Card.Link>
+                                    </Card.Body>
+                                </Card>{" "}
+                            </div>
+                            <div className='aboutcard_2'>
+                                <Card style={{ width: "18rem" }}>
+                                    <Card.Img variant='top' src={Team1} alt='team1' />
+                                    <Card.Body>
+                                        <Card.Title>Maria Vlasopoulou</Card.Title>
+                                        <Card.Text>Full Stack Developer</Card.Text>
+                                        <Card.Link href='https://www.linkedin.com/in/maria-vlasopoulou-20527b162/'>Linkedin</Card.Link>
+                                        <Card.Link href='https://github.com/vlasmar'>Gith hub</Card.Link>
+                                    </Card.Body>
+                                </Card>{" "}
+                            </div>
+                        </div>
                     </div>
                 }
             </div>
-
             <Footer />
         </div>
     );
