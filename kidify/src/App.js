07fe8ca1cd6353fcf427154c86cd068e01a38745
vlasmar <40355727+vlasmar@users.noreply.github.com>
2022-10-17 13:56:12 +0200

    Update App.js

--- kidify/src/App.js
+++ kidify/src/App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from './Components/Homepage';
 import About from "./Components/About";
-// import Signup from './Components/Signup';
 import UserProfile from "./Components/UserProfile";
 
 function App() {
@@ -14,9 +13,6 @@ function App() {
           <Route path="/" element={<Homepage />} />
           <Route path="/about" element={<About />} />
            <Route path='/username' element={<UserProfile />} />
-
-          {/*  
-          <Route path="/signup" element={<Signup />} />  
         </Routes>
       </BrowserRouter>
     </div>
