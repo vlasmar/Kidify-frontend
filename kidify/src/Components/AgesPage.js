ad5e06c9654c03e778c2e35f72abc130d2d81cdf
Priyanka_Bed <108406975+PriyankaBed@users.noreply.github.com>
2022-10-18 12:56:25 +0200

    added AgesPage.js
    added AgesPage.js

---
+++ kidify/src/Components/AgesPage.js
@@ -0,0 +1,18 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
+function AgesPage() {
+    return (
+        <div className='agespage'>
+            <div className='header'>
+                <Header />
+            </div>
+            <h1>Preschool Activities 0-3</h1>
+            <div className='footer'>
+                <Footer />
+            </div>
+        </div>
+    );
+}
+
+export default AgesPage;
