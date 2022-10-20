624ba0f2406edb934dda5481c9e0f60302be2303
Priyanka_Bed <108406975+PriyankaBed@users.noreply.github.com>
2022-10-20 13:25:48 +0200

    added home update
    added home update

--- kidify/src/Components/Homepage.js
+++ kidify/src/Components/Homepage.js
@@ -1,29 +1,30 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import { Container, Col, Row, Button } from "react-bootstrap";
 
 function Homepage({ data }) {
-  return (
-    <div className="homepage">
-      <div className="header">
-        <Header />
-      </div>
-      <div className="container">
-        <div className="video-cards">
-          {data.map((dataItem) => {
-            return (
-              <div key={dataItem.title}>
-                <img src={dataItem.video_img_url} />
-                <h4>{dataItem.title}</h4>
-              </div>
-            );
-          })}
+    return (
+        <div className='homepage'>
+            <div className='header'>
+                <Header />
+                <div className='video-cards'>
+                    {data.map((dataItem) => {
+                        return (
+                            <div className='card-continer'>
+                                <div key={dataItem.title}>
+                                    <img src={dataItem.video_img_url} />
+                                    <h4>{dataItem.title}</h4>
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
+                <div className='footer'>
+                    <Footer />
+                </div>
+            </div>
         </div>
-      </div>
-      <div className="footer">
-        <Footer />
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Homepage;
