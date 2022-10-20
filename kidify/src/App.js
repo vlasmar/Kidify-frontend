446d58ef2c26a522dbfb223c90648aa637668e85
Priyanka_Bed <108406975+PriyankaBed@users.noreply.github.com>
2022-10-20 13:33:52 +0200

    added app.js update
    added app.js update

--- kidify/src/App.js
+++ kidify/src/App.js
@@ -5,38 +5,38 @@ import axios from "axios";
 import Homepage from "./Components/Homepage";
 import About from "./Components/About";
 import Profile from "./Components/Profile";
-import Page03 from "./Components/Page03";
-import Page45 from "./Components/Page45";
-import Page67 from "./Components/Page67";
-import AdminPage from "./Components/AdminPage";
+import Page03 from "./Components/Categories/Page03";
+import Page45 from "./Components/Categories/Page45";
+import Page67 from "./Components/Categories/Page67";
+import AdminPage from "./Components/AdminPage/AdminPage";
 
 function App() {
-  const [data, setData] = useState([]);
+    const [data, setData] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get(`http://localhost:4000/videos`)
-      .then((response) => {
-        setData(response.data);
-      })
-      .catch((err) => alert(err));
-  }, []);
+    useEffect(() => {
+        axios
+            .get(`http://localhost:4000/videos`)
+            .then((response) => {
+                setData(response.data);
+            })
+            .catch((err) => alert(err));
+    }, []);
 
-  return (
-    <div className="wrapper">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Homepage data={data} />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/:username" element={<Profile />} />
-          <Route path="/preschool" element={<Page03 data={data} />} />
-          <Route path="/grade-k" element={<Page45 data={data} />} />
-          <Route path="/grade-1" element={<Page67 data={data} />} />
-          <Route path="/upload_video" element={<AdminPage />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className='wrapper'>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Homepage data={data} />} />
+                    <Route path='/about' element={<About />} />
+                    <Route path='/:username' element={<Profile />} />
+                    <Route path='/preschool' element={<Page03 data={data} />} />
+                    <Route path='/grade-k' element={<Page45 data={data} />} />
+                    <Route path='/grade-1' element={<Page67 data={data} />} />
+                    <Route path='/upload_video' element={<AdminPage />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
