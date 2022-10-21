46b9dfb387abc4a24a7a38aa00986836884bd975
Priyanka_Bed <108406975+PriyankaBed@users.noreply.github.com>
2022-10-21 11:03:42 +0200

    added home.js file
    added home.js file

--- kidify/src/Components/Homepage.js
+++ kidify/src/Components/Homepage.js
@@ -1,32 +1,38 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import { NavLink } from "react-router-dom";
+import AddToPlaylist from "../Images/add_to_playlist.png";
 
 function Homepage({ data }) {
-  return (
-    <div className="homepage">
-      <div className="header">
-        <Header />
-      </div>
-      <div className="container">
-        <div className="video-cards">
-          {data.map((dataItem) => {
-            return (
-              <div className="card-container">
-                <NavLink to={`/player/${dataItem._id}`}>
-              <div key={dataItem.title}>
-                <img src={dataItem.video_img_url} alt={dataItem.title}/>
-                <h4>{dataItem.title}</h4>
-              </div>
-              </NavLink>
-              </div>
-            );
-          })}
-          </div>
-          </div>
-          <div className='footer'>
-             <Footer />
-          </div>
+    return (
+        <div className='homepage'>
+            <div className='header'>
+                <Header />
+            </div>
+            <div className='container'>
+                <div className='video-cards'>
+                    {data.map((dataItem) => {
+                        return (
+                            <div className='card-container'>
+                                <NavLink to={`/player/${dataItem._id}`}>
+                                    <div key={dataItem.title}>
+                                        <img src={dataItem.video_img_url} alt={dataItem.title} />
+                                        <div className='cardbody'>
+                                            <h4>{dataItem.title}</h4>
+                                            <div className='playlist_icon'>
+                                                <img src={AddToPlaylist} alt='favorite' />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </NavLink>
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+            <div className='footer'>
+                <Footer />
+            </div>
         </div>
     );
 }
