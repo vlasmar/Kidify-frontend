a96e471975ffe1050551ff358c854448cca19bf0
Priyanka_Bed <108406975+PriyankaBed@users.noreply.github.com>
2022-10-27 11:05:46 +0200

    update code
    update code

--- kidify/src/Components/Categories/Page45.js
+++ kidify/src/Components/Categories/Page45.js
@@ -8,18 +8,22 @@ function Page45({ data }) {
             <div className='header'>
                 <Header />
             </div>
-            <div className='video-cards'>
-                {data
-                    .filter((item) => item.category === "4-5")
-                    .map((item) => {
-                        return (
-                            <div className='card-container-45 card-container'>
-                                <div>
-                                   <VideoItem video={item}/>
+            <div className='container'>
+                <h1 style={{ fontFamily: "cursive", margin: "20px" }}>Ages 4-5</h1>
+
+                <div className='video-cards'>
+                    {data
+                        .filter((item) => item.category === "4-5")
+                        .map((item) => {
+                            return (
+                                <div className='card-container-45 card-container'>
+                                    <div>
+                                        <VideoItem video={item} />
+                                    </div>
                                 </div>
-                            </div>
-                        );
-                    })}
+                            );
+                        })}
+                </div>
             </div>
             <div className='footer'>
                 <Footer />
