8125a3df65a5c5c15058f8529fbc950b7227585f
Priyanka_Bed <108406975+PriyankaBed@users.noreply.github.com>
2022-10-27 11:05:16 +0200

    update code
    update code

--- kidify/src/Components/Categories/Page03.js
+++ kidify/src/Components/Categories/Page03.js
@@ -3,29 +3,32 @@ import Footer from "../Footer";
 import VideoItem from "../VideoItem";
 
 function Page03({ data }) {
-  return (
-    <div className="agespage">
-      <div className="header">
-        <Header />
-      </div>
-      <div className="video-cards">
-        {data
-          .filter((item) => item.category === "0-3")
-          .map((item) => {
-            return (
-              <div className="card-container-03 card-container">
-                <div>
-                  <VideoItem video={item}/>
+    return (
+        <div className='agespage'>
+            <div className='header'>
+                <Header />
+            </div>
+            <div className='container'>
+                <h1 style={{ fontFamily: "cursive", margin: "20px" }}>Ages 0-3</h1>
+                <div className='video-cards'>
+                    {data
+                        .filter((item) => item.category === "0-3")
+                        .map((item) => {
+                            return (
+                                <div className='card-container-03 card-container'>
+                                    <div>
+                                        <VideoItem video={item} />
+                                    </div>
+                                </div>
+                            );
+                        })}
                 </div>
-              </div>
-            );
-          })}
-      </div>
-      <div className="footer">
-        <Footer />
-      </div>
-    </div>
-  );
+            </div>
+            <div className='footer'>
+                <Footer />
+            </div>
+        </div>
+    );
 }
 
 export default Page03;
